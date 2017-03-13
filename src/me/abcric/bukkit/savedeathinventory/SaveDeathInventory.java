3f101535439b45fb188db716380939cb208654aa
ABCRic <ABCRic@users.noreply.github.com>
2017-03-13 13:41:48 +0000

    Add plugin skeleton, use Maven for dependencies

---
+++ src/me/abcric/bukkit/savedeathinventory/SaveDeathInventory.java
@@ -0,0 +1,15 @@
+package me.abcric.bukkit.savedeathinventory;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SaveDeathInventory extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+
+	@Override
+	public void onDisable() {
+		
+	} 
+}
