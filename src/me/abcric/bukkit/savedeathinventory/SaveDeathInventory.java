73b1887b46d83bbf0ff9f625ff6c311d5af8e143
ABCRic <ABCRic@users.noreply.github.com>
2017-03-14 23:41:43 +0000

    Add startup message, save inventory on death

--- src/me/abcric/bukkit/savedeathinventory/SaveDeathInventory.java
+++ src/me/abcric/bukkit/savedeathinventory/SaveDeathInventory.java
@@ -5,7 +5,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class SaveDeathInventory extends JavaPlugin {
 	@Override
 	public void onEnable() {
-		
+		getLogger().info("Starting up " + getDescription().getName() + " " + getDescription().getVersion() + " by "
+				+ String.join(", ", getDescription().getAuthors()) + "...");
+		getServer().getPluginManager().registerEvents(new SaveDeathInventoryListener(this), this);
+		getLogger().info("Enabled.");
 	}
 
 	@Override
---
+++ src/me/abcric/bukkit/savedeathinventory/SaveDeathInventoryListener.java
@@ -0,0 +1,54 @@
+package me.abcric.bukkit.savedeathinventory;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class SaveDeathInventoryListener implements Listener {
+	SaveDeathInventory plugin;
+
+	public SaveDeathInventoryListener(SaveDeathInventory plugin) {
+		this.plugin = plugin;
+	}
+
+	static String CurrentTimeString() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+		Date now = new Date();
+		return sdf.format(now);
+	}
+
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent event) {
+		plugin.getLogger().info("Player death: " + event.getDeathMessage());
+
+		Player p = event.getEntity();
+
+		/* dump inventory to file */
+		YamlConfiguration config = new YamlConfiguration();
+		config.set("inventory", p.getInventory().getContents());
+		config.set("armor", p.getInventory().getArmorContents());
+
+		String filename = plugin.getDataFolder() + File.separator + CurrentTimeString() + "_" + p.getUniqueId()
+				+ ".yml";
+
+		try {
+			if (!plugin.getDataFolder().exists())
+				plugin.getDataFolder().mkdir();
+			File f = new File(filename);
+			if (!f.exists())
+				f.createNewFile();
+			config.save(f);
+			plugin.getLogger().info("Inventory saved to " + f.getAbsolutePath());
+		} catch (IOException e) {
+			plugin.getLogger().severe("Could not save player inventory to file " + filename + ": " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
